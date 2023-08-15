316bd8953c2afd397179c5a954f357489dbc3dcf
ispong <ispong@outlook.com>
2023-08-15 11:09:13 +0800

    :tada: isx开发脚手架

---
+++ README.md
@@ -0,0 +1,8 @@
+#### 安装命令
+
+> 仅限mac系统
+> 需要提前安装 zsh
+
+```bash
+zsh install.sh
+```
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 获取当前路径
+BASE_PATH=$(cd "$(dirname "$0")" || exit ; pwd)
+
+# 安装nuitka
+python3 -m pip install nuitka
+
+# 安装依赖
+python3 -m pip install -r requirements.txt
+
+# 清空打包文件
+rm -rf ${BASE_PATH}/build
+rm -f isx
+
+# 打包
+python3 -m nuitka --onefile --output-dir=./build --output-filename=isx main.py
+
+# 配置环境变量
+echo 'export PATH=$PATH:'"${BASE_PATH}" >> ~/.zshrc
+
+# 生效配置
+source ~/.zshrc
