d57f88dd3876b86a7cd78e5091dfba5ba059b139
ispong <ispong@outlook.com>
2023-09-13 17:51:33 +0800

    :sparkles: 修复branch如果本地有分支，直接切换

--- action/branch.py
+++ action/branch.py
@@ -13,7 +13,7 @@ import os
 
 
 def get_branch(num, origin):
-    git_command = "git branch -r -l " + origin + "/*-#" + num
+    git_command = "git branch -r -l " + origin + "*-#" + num
     result = subprocess.run(git_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
                             cwd=get_current_project_path())
     branches = result.stdout.split('\n')
@@ -38,29 +38,34 @@ def branch():
     check_current_project()
     exec_command("cd " + get_current_project_path() + " && git fetch upstream && git fetch origin")
     exec_command("cd " + get_current_project_vip_path() + " && git fetch upstream && git fetch origin")
-    branch_name = get_branch(get_branch_num(), "origin")
+    branch_name = get_branch(get_branch_num(), "")
     if branch_name != '':
-        print("切换分支：" + branch_name)
-        exec_git_command("git checkout --track origin/" + branch_name)
+        exec_command("cd " + get_current_project_path() + " && git checkout " + branch_name)
+        exec_command("cd " + get_current_project_vip_path() + " && git checkout " + branch_name)
     else:
-        branch_name = get_branch(get_branch_num(), "upstream")
+        branch_name = get_branch(get_branch_num(), "/origin")
         if branch_name != '':
             print("切换分支：" + branch_name)
-            exec_git_command("git checkout --track upstream/" + branch_name)
+            exec_git_command("git checkout --track origin/" + branch_name)
         else:
-            # 如果自己仓库和upstream仓库都没有分支，则需要创建分支
-            url = "https://api.github.com/repos/isxcode/spark-yun/releases/latest"
-            headers = {
-                "Accept": "application/vnd.github+json",
-                "Authorization": "Bearer " + get_token(),
-                "X-GitHub-Api-Version": "2022-11-28"
-            }
-            response = requests.get(url, headers=headers)
-            if response.status_code == 401:
-                print("github token权限不足，请重新登录")
+            branch_name = get_branch(get_branch_num(), "/upstream")
+            if branch_name != '':
+                print("切换分支：" + branch_name)
+                exec_git_command("git checkout --track upstream/" + branch_name)
             else:
-                release_name = json.loads(response.text)['name'].replace("v", '')
-                branch_name = release_name + "-#" + get_branch_num()
-                print("创建分支：" + branch_name)
-                print("请从github(https://github.com/isxcode/spark-yun) 中创建需求分支：" + branch_name)
-                exec_git_command("git checkout -b " + branch_name + " upstream/latest")
+                # 如果自己仓库和upstream仓库都没有分支，则需要创建分支
+                url = "https://api.github.com/repos/isxcode/spark-yun/releases/latest"
+                headers = {
+                    "Accept": "application/vnd.github+json",
+                    "Authorization": "Bearer " + get_token(),
+                    "X-GitHub-Api-Version": "2022-11-28"
+                }
+                response = requests.get(url, headers=headers)
+                if response.status_code == 401:
+                    print("github token权限不足，请重新登录")
+                else:
+                    release_name = json.loads(response.text)['name'].replace("v", '')
+                    branch_name = release_name + "-#" + get_branch_num()
+                    print("创建分支：" + branch_name)
+                    print("请从github(https://github.com/isxcode/spark-yun) 中创建需求分支：" + branch_name)
+                    exec_git_command("git checkout -b " + branch_name + " upstream/latest")
