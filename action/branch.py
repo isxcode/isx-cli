c6f654bc501941871856b522f7f4cfe5c458714a
ispong <ispong@outlook.com>
2023-10-13 11:34:36 +0800

    :art: 优化branch命令

--- action/branch.py
+++ action/branch.py
@@ -11,12 +11,8 @@ from config import get_config
 from config import get_token
 
 
-def get_branch(branch_num, project_dir, prefix):
-    if prefix != 'local':
-        prefix = prefix + '/'
-    else:
-        prefix = ''
-    search_git_command = "git branch -l " + prefix + "*-#" + branch_num
+def get_local_branch(branch_num, project_dir):
+    search_git_command = "git branch -l *-#" + branch_num
     result = subprocess.run(search_git_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
                             cwd=project_dir)
     branches = result.stdout.split('\n')
@@ -24,6 +20,18 @@ def get_branch(branch_num, project_dir, prefix):
         branch_sub = branch_meta.strip().split('-')
         if len(branch_sub) > 1 and branch_sub[1] == "#" + branch_num:
             return branch_meta.replace("*", '').strip()
+    return ""
+
+
+def get_remote_branch(branch_num, project_dir, prefix):
+    search_git_command = "git branch -r -l " + prefix + "/*-#" + branch_num
+    result = subprocess.run(search_git_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
+                            cwd=project_dir)
+    branches = result.stdout.split('\n')
+    for branch_meta in branches:
+        branch_sub = branch_meta.strip().split('-')
+        if len(branch_sub) > 1 and branch_sub[1] == "#" + branch_num:
+            return branch_meta.strip().replace('origin/', '')
     return ""
 
 
@@ -44,14 +52,31 @@ def fetch_branch(project_dir, sub_repository):
 
 
 def push_branch(project_dir, sub_repository, branch_name, prefix):
-    exec_command("cd " + project_dir + " && git push origin " + branch_name)
+    exec_command("cd " + project_dir + " && git push " + prefix + " " + branch_name)
     for repository_meta in sub_repository:
         matcher = re.search(r"/([^/.]+)\.git", repository_meta)
         project_name = matcher.group(1)
         exec_command("cd " + project_dir + '/' + project_name + " && git push " + prefix + " " + branch_name)
 
 
-def get_latest_branch():
+def get_latest_branch_name(branch_num, project_name):
+    url = "https://api.github.com/repos/isxcode/" + project_name + "/issues/" + branch_num
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer " + get_token(),
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 401:
+        print("github token权限不足，请重新登录")
+        exit(0)
+    elif response.status_code == 404:
+        print("issue编号不存在，请输入合法的issue编号")
+        exit(0)
+    elif response.status_code != 200:
+        print("issue异常，请检查issue合法性")
+        exit(0)
+
     url = "https://api.github.com/repos/isxcode/spark-yun/releases/latest"
     headers = {
         "Accept": "application/vnd.github+json",
@@ -70,37 +95,45 @@ def get_latest_branch():
 def branch():
     isx_config = get_config()
     check_current_project(isx_config)
-    project_info = isx_config['projects'][isx_config['develop-project']]
+    project_name = isx_config['develop-project']
+    project_info = isx_config['projects'][project_name]
     project_dir = project_info['dir']
     sub_repository = list(project_info['sub-repository'])
     fetch_branch(project_dir, sub_repository)
     branch_num = get_branch_num()
 
     # 从本地拉分支
-    branch_name = get_branch(branch_num, project_dir, 'local')
+    branch_name = get_local_branch(branch_num, project_dir)
     if branch_name != '':
         checkout_branch(project_dir, sub_repository, branch_name)
-        print("分支名:" + branch_name)
+        print("====================================")
+        print("当前开发分支 ==> " + branch_name)
+        print("====================================")
         exit(0)
 
     # 从个人仓库拉分支
-    branch_name = get_branch(branch_num, project_dir, 'origin')
+    branch_name = get_remote_branch(branch_num, project_dir, 'origin')
     if branch_name != '':
-        branch_name_command = " -b " + branch_name + " origin/" + branch_name
+        branch_name_command = "-b " + branch_name + " origin/" + branch_name
         checkout_branch(project_dir, sub_repository, branch_name_command)
-        print("分支名:" + branch_name)
+        print("====================================")
+        print("当前开发分支 ==> " + branch_name)
+        print("====================================")
         exit(0)
 
     # 从upstream远程仓库拉分支
-    branch_name = get_branch(branch_num, project_dir, 'upstream')
+    branch_name = get_remote_branch(branch_num, project_dir, 'upstream')
     if branch_name != '':
-        branch_name_command = " -b " + branch_name + " upstream/" + branch_name
+        branch_name_command = "-b " + branch_name + " upstream/" + branch_name
         checkout_branch(project_dir, sub_repository, branch_name_command)
-        push_branch(branch_name, 'origin')
-        print("分支名:" + branch_name)
+        push_branch(branch_name, sub_repository, branch_name, 'origin')
     else:
-        branch_name_command = " -b " + get_latest_branch() + " upstream/latest"
+        branch_name = get_latest_branch_name(branch_num, project_name)
+        branch_name_command = " -b " + branch_name + " upstream/latest"
         checkout_branch(project_dir, sub_repository, branch_name_command)
-        push_branch(branch_name, 'origin')
-        push_branch(branch_name, 'upstream')
-        print("分支名:" + branch_name)
+        push_branch(project_dir, sub_repository, branch_name, 'origin')
+        push_branch(project_dir, sub_repository, branch_name, 'upstream')
+    print("====================================")
+    print("当前开发分支 ==> " + branch_name)
+    print("====================================")
+    exit(0)
--- command.py
+++ command.py
@@ -2,13 +2,12 @@ import subprocess
 
 
 def exec_command(command):
-    print("执行命令：" + command)
+    print("执行命令【 " + command + " 】")
     completed_process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     if completed_process.returncode == 0:
         if completed_process.stdout != '':
-            print('命令输出：' + completed_process.stdout)
+            print('stdout ==> ' + completed_process.stdout[:-1])
         if completed_process.stderr != '':
-            print('命令输出：' + completed_process.stderr)
-        print('执行命令成功')
+            print('stderr ==> ' + completed_process.stderr[:-1])
     else:
         print(completed_process.stderr + "执行失败")
