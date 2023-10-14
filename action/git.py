25c03a4ec1cc114f48217c0619dc985c8bda4e89
ispong <ispong@outlook.com>
2023-10-14 16:50:53 +0800

    :bug: 修复git命令无法使用

--- action/git.py
+++ action/git.py
@@ -1,16 +1,23 @@
+import re
+
 from args import get_git_command
-from config import check_current_project
-from config import get_current_project_vip_path
-from config import get_current_project_path
-import os
+from config import get_config
+from command import exec_command
 
 
 def git():
-    check_current_project()
+    isx_config = get_config()
+    project_name = isx_config['develop-project']
+    project_info = isx_config['projects'][project_name]
+    sub_repository = list(project_info['sub-repository'])
+    project_dir = project_info['dir']
     git_command = get_git_command()
-    command = "cd " + get_current_project_path() + " && " + git_command
-    os.system(command)
-    command = "cd " + get_current_project_vip_path() + " && " + git_command
-    os.system(command)
-    command = "cd " + get_current_project_path()
-    os.system(command)
+    command = "cd " + project_dir + " && " + git_command
+    exec_command(command)
+    for repository in sub_repository:
+        matcher = re.search(r"/([^/.]+)\.git", repository)
+        project_name = matcher.group(1)
+        command = "cd " + project_dir + "/" + project_name + " && " + git_command
+        exec_command(command)
+    command = "cd " + project_dir
+    exec_command(command)
