429d040b285ca762afe54ffc93c8fa3e88d9112e
ispong <ispong@outlook.com>
2024-01-16 16:00:26 +0800

    修改install中的版本号

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo wget https://github.com/isxcode/isx-cli/releases/download/v0.0.5/isx_darwin_arm64 -O /usr/local/bin/isx
+sudo wget https://github.com/isxcode/isx-cli/releases/download/v1.0.0/isx_darwin_arm64 -O /usr/local/bin/isx
 sudo chmod a+x /usr/local/bin/isx
 
 echo "安装成功"
