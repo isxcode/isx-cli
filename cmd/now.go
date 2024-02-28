e0e95c17f7e92825641d9cae35eb493a216aa0c9
ispong <ispong@outlook.com>
2024-02-28 16:20:19 +0800

    修复isx now

--- cmd/now.go
+++ cmd/now.go
@@ -8,6 +8,7 @@ import (
 	"github.com/isxcode/isx-cli/git"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
 )
 
 func init() {
@@ -25,6 +26,12 @@ var nowCmd = &cobra.Command{
 
 func nowCmdMain() {
 	projectName := viper.GetString("current-project.name")
+
+	if projectName == "" {
+		fmt.Println("请先选择项目开发")
+		os.Exit(1)
+	}
+
 	projectPath := viper.GetString(projectName+".dir") + "/" + projectName
 	branchName := git.GetCurrentBranchName(projectName, false)
 
