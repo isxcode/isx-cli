c72c942b01e593ea635141802a9ab5ae1fd5456c
ispong <ispong@outlook.com>
2024-02-28 18:09:06 +0800

    GH-93 修复upgrade下载的命令无法使用

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo wget https://github.com/isxcode/isx-cli/releases/download/v1.1.0/isx_darwin_arm64 -O /usr/local/bin/isx
+sudo wget https://github.com/isxcode/isx-cli/releases/download/v1.1.0/isx_darwin_arm64 -O /usr/local/bin/isx_darwin_arm64
+sudo mv /usr/local/bin/isx_darwin_arm64 /usr/local/bin/isx
 sudo chmod a+x /usr/local/bin/isx
 
 echo "安装成功"
