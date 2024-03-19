559fb5e95a736a143f93d204e8d814f8e4209380
ispong <ispong@outlook.com>
2024-03-19 10:34:32 +0800

    GH-98 直接使用mv更新，没有更新版本号
    * :bug: upgrade命令 更新版本号
    
    * :bug: upgrade命令 更新版本号
    
    ---------
    
    Co-authored-by: EchoJamie <myz0104@163.com>

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 sudo wget https://github.com/isxcode/isx-cli/releases/download/v1.1.0/isx_darwin_arm64 -O /usr/local/bin/isx_darwin_arm64
 sudo mv /usr/local/bin/isx_darwin_arm64 /usr/local/bin/isx
 sudo chmod a+x /usr/local/bin/isx
+/usr/local/bin/isx set version.number 1.1.0
 
 echo "安装成功"
