df9c215dc2ff2af553305d487915d62e335794e1
ispong <ispong@outlook.com>
2024-05-22 17:43:27 +0800

    GH-138 自动打包推送到阿里云oss中

--- install.sh
+++ install.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 
-sudo curl -ssL https://isxcode.oss-cn-shanghai.aliyuncs.com/zhixingyun/isx_darwin_arm64 -o /usr/local/bin/isx_darwin_arm64
-sudo mv /usr/local/bin/isx_darwin_arm64 /usr/local/bin/isx
-sudo chmod a+x /usr/local/bin/isx
+ARCH=$(uname -m)
+OS_TYPE=$OSTYPE
+
+if [[ "$OS_TYPE" == "linux-gnu"* ]]; then
+    if [ "$ARCH" == "x86_64" ]; then
+        sudo curl -ssL https://isxcode.oss-cn-shanghai.aliyuncs.com/zhixingyun/isx_linux_amd64 -o /usr/local/bin/isx_linux_amd64
+        sudo mv /usr/local/bin/isx_linux_amd64 /usr/local/bin/isx
+        sudo chmod a+x /usr/local/bin/isx
+    fi
+elif [[ "$OS_TYPE" == "darwin"* ]]; then
+    if [ "$ARCH" == "x86_64" ]; then
+        sudo curl -ssL https://isxcode.oss-cn-shanghai.aliyuncs.com/zhixingyun/isx_darwin_amd64 -o /usr/local/bin/isx_darwin_amd64
+        sudo mv /usr/local/bin/isx_darwin_amd64 /usr/local/bin/isx
+        sudo chmod a+x /usr/local/bin/isx
+    elif [ "$ARCH" == "aarch64" ]; then
+        sudo curl -ssL https://isxcode.oss-cn-shanghai.aliyuncs.com/zhixingyun/isx_darwin_arm64 -o /usr/local/bin/isx_darwin_arm64
+        sudo mv /usr/local/bin/isx_darwin_arm64 /usr/local/bin/isx
+        sudo chmod a+x /usr/local/bin/isx
+    fi
+else
+    echo "Unsupported OS or architecture: $OS_TYPE / $ARCH"
+    exit 1
+fi
 
 echo "安装成功"
