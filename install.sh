85a01b074778c534273f3a39321762ffe580985f
ispong <ispong@outlook.com>
2024-06-09 17:20:50 +0800

    windows不需要sudo

--- install.sh
+++ install.sh
@@ -37,6 +37,6 @@ else
     echo "开始下载"
     curl -ssL https://isxcode.oss-cn-shanghai.aliyuncs.com/zhixingyun/isx_windows_amd64.exe -o /usr/bin/isx.exe
     echo "下载完成"
-    chmod a+x /usr/local/bin/isx
+    chmod a+x /usr/bin/isx.exe
     echo "安装成功"
 fi
