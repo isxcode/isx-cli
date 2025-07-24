37cdf7977f0f966e7d29707f25360b010a7c0e83
ispong <ispong@outlook.com>
2025-07-24 22:21:11 +0800

    优化isx checkout命令

--- cmd/push.go
+++ cmd/push.go
@@ -28,10 +28,49 @@ var pushCmd = &cobra.Command{
 }
 
 func pushCmdMain() {
+	// 获取当前项目名称 - 支持新旧配置格式
+	projectName := viper.GetString("now-project")
+	if projectName == "" {
+		projectName = viper.GetString("current-project.name")
+	}
 
-	projectName := viper.GetString("current-project.name")
-	projectDir := viper.GetString(projectName + ".dir")
-	projectPath := projectDir + "/" + projectName
+	if projectName == "" {
+		fmt.Println("请先使用【isx choose】选择项目")
+		os.Exit(1)
+	}
+
+	// 获取项目路径 - 支持新旧配置格式
+	var projectPath string
+
+	// 尝试新配置格式：从 project-list 数组中查找
+	type ProjectConfig struct {
+		Name string `mapstructure:"name"`
+		Dir  string `mapstructure:"dir"`
+	}
+
+	var projectList []ProjectConfig
+	configErr := viper.UnmarshalKey("project-list", &projectList)
+	if configErr == nil {
+		for _, proj := range projectList {
+			if proj.Name == projectName {
+				projectPath = proj.Dir
+				break
+			}
+		}
+	}
+
+	// 如果新配置格式没找到，尝试旧配置格式
+	if projectPath == "" {
+		projectDir := viper.GetString(projectName + ".dir")
+		if projectDir != "" {
+			projectPath = projectDir + "/" + projectName
+		}
+	}
+
+	if projectPath == "" {
+		fmt.Printf("项目 %s 未下载，请先使用【isx clone】下载项目代码\n", projectName)
+		os.Exit(1)
+	}
 
 	// 除了isx-cli项目，其他都要使用gradle 格式化代码
 	if "isx-cli" != projectName && "tools-yun" != projectName {
